bc9b6d3bb3a1084d9b94bf16f50cb197ed6c8737
Artur Meinen <arturmeinen@gmail.com>
2019-08-02 14:57:24 -0300

    cm1

--- steps/steps.py
+++ steps/steps.py
@@ -16,11 +16,11 @@ def step_impl(context):
     context.obj.acessar_site()
 
 
-@when('seleciono a aba oportunidades verificando vagas de desenvolvedor')
+@when('seleciono a aba oportunidades verificando as vagas disponiveis')
 def step_impl(context):
     context.obj.acessar_oportunidades()
 
 
-@then('as vagas de desenvolvedor são encontradas com sucesso')
-def step_impl(context):
-    assert context.obj.validar_vaga_dev() is True
+@then('as vagas de {tipo_vaga} são encontradas com sucesso')
+def step_impl(context, tipo_vaga):
+    assert context.obj.validar_vaga_dev(tipo_vaga) is True
