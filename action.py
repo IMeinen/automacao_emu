e25342cbd1452d3697574be4fe037425e8a5b80d
Artur Meinen <arturmeinen@gmail.com>
2019-08-05 17:42:28 -0300

    cm9

--- action.py
+++ action.py
@@ -21,7 +21,7 @@ class MinhaAction(AcessarGoogle):
         """ O método 'acessar_oportunidades' acessa a aba de 'Oportunidades' no site da Zallpy """
         self.selecionar_oportunidades()
 
-    def validar_vaga_dev(self,tipo_vaga) -> bool:
+    def validar_vaga_dev(self, tipo_vaga) -> bool:
         """ A função 'validar_vaga_dev' recebe a lista com as vagas disponíveis,
          e retorna 'True' se encontrou alguma vaga de 'tipo_vaga' """
         lista = self.selecionar_vagas_disponiveis()
@@ -29,3 +29,19 @@ class MinhaAction(AcessarGoogle):
             return True
         else:
             print('Vagas de {} não encontradas!'.format(tipo_vaga))
+
+    def validar_vagas(self) -> bool:
+
+        lista = self.selecionar_vagas_disponiveis()
+        if any("designer" in texto.lower() for texto in lista):
+            print('Vagas de designer disponíveis.')
+            return True
+        elif any("desenvolvedor" in texto.lower() for texto in lista):
+            print('Vagas de desenvolvedor disponíveis.')
+            return True
+        elif any("estagiário" in texto.lower() for texto in lista):
+            print('Vagas de estagiário disponíveis')
+            return True
+        else:
+            print('Vagas indisponíveis no momento.')
+            return False
